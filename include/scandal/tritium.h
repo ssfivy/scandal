7b8ce5a2329fa094256e672e9ab0bab1e91a81a5
Etienne Le Sueur <elesueur@cse.unsw.edu.au>
2011-09-02 00:36:39 +1000

    added tritium header

---
+++ include/scandal/tritium.h
@@ -0,0 +1,67 @@
+/*
+ * Tritium TRI63 CAN Driver Controls header
+ * Copyright (c) 2006, Tritium Pty Ltd.  All rights reserved.
+ *  
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *	- Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer 
+ *	  in the documentation and/or other materials provided with the distribution.
+ *	- Neither the name of Tritium Pty Ltd nor the names of its contributors may be used to endorse or promote products 
+ *	  derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+ * IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+ * OF SUCH DAMAGE.
+ *
+ * Last Modified: D. Snowdon, 31 June, 2008
+ *
+ */
+
+#ifndef __TRITIUM_H__
+#define __TRITIUM_H__
+
+// Constant Definitions
+#define	TRUE			1
+#define FALSE			0
+
+// Typedefs for quickly joining multiple bytes/ints/etc into larger values
+// These rely on byte ordering in CPU & memory - i.e. they're not portable across architectures
+typedef union _group_64 {
+	float data_fp[2];
+	unsigned char data_u8[8];
+	unsigned int data_u16[4];
+	unsigned long data_u32[2];
+} group_64;
+
+typedef union _group_32 {
+	float data_fp;
+	unsigned char data_u8[4];
+	unsigned int data_u16[2];
+	unsigned long data_u32;
+} group_32;
+
+typedef union _group_16 {
+	unsigned char data_u8[2];
+	unsigned int data_u16;
+} group_16;
+
+// Driver controls CAN base address and packet offsets
+#define DC_CAN_BASE		0x0500
+#define DC_DRIVE		0x01
+#define DC_POWER		0x02
+#define DC_RESET		0x03
+#define DC_SWITCH		0x04
+
+// Status values (for message reception)
+#define CAN_ERROR		0xFFFF
+#define CAN_MERROR		0xFFFE
+#define CAN_WAKE		0xFFFD
+#define CAN_RTR			0xFFFC
+#define CAN_OK			0x0001
+
+#endif

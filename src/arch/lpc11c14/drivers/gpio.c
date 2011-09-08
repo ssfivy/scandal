6f34eb60a208297f4315200efa3b2a6e561823c1
David Favaloro dfavaloro <David Favaloro dfavaloro@gmail.com>
2011-09-08 23:55:42 +1000

    some work on the various PCBs

---
+++ src/arch/lpc11c14/drivers/gpio.c
@@ -0,0 +1,505 @@
+/****************************************************************************
+ *   $Id:: gpio.c 4785 2010-09-03 22:39:27Z nxp21346                        $
+ *   Project: NXP LPC11xx GPIO example
+ *
+ *   Description:
+ *     This file contains GPIO code example which include GPIO 
+ *     initialization, GPIO interrupt handler, and related APIs for 
+ *     GPIO access.
+ *
+ ****************************************************************************
+ * Software that is described herein is for illustrative purposes only
+ * which provides customers with programming information regarding the
+ * products. This software is supplied "AS IS" without any warranties.
+ * NXP Semiconductors assumes no responsibility or liability for the
+ * use of the software, conveys no license or title under any patent,
+ * copyright, or mask work right to the product. NXP Semiconductors
+ * reserves the right to make changes in the software without
+ * notification. NXP Semiconductors also make no representation or
+ * warranty that such application will be suitable for the specified
+ * use without further testing or modification.
+****************************************************************************/
+#include <project/driver_config.h>
+
+#if CONFIG_ENABLE_DRIVER_GPIO==1
+
+#include <arch/gpio.h>
+
+#if CONFIG_GPIO_DEFAULT_PIOINT0_IRQHANDLER==1
+volatile uint32_t gpio0_counter = 0;
+volatile uint32_t p0_1_counter  = 0;
+/*****************************************************************************
+** Function name:		PIOINT0_IRQHandler
+**
+** Descriptions:		Use one GPIO pin(port0 pin1) as interrupt source
+**
+** parameters:			None
+** Returned value:		None
+** 
+*****************************************************************************/
+void PIOINT0_IRQHandler(void)
+{
+  uint32_t regVal;
+
+  gpio0_counter++;
+  regVal = GPIOIntStatus( PORT0, 1 );
+  if ( regVal )
+  {
+	p0_1_counter++;
+	GPIOIntClear( PORT0, 1 );
+  }		
+  return;
+}
+#endif
+
+#if CONFIG_GPIO_DEFAULT_PIOINT1_IRQHANDLER==1
+volatile uint32_t gpio1_counter = 0;
+volatile uint32_t p1_1_counter  = 0;
+/*****************************************************************************
+** Function name:		PIOINT1_IRQHandler
+**
+** Descriptions:		Use one GPIO pin(port1 pin1) as interrupt source
+**
+** parameters:			None
+** Returned value:		None
+** 
+*****************************************************************************/
+void PIOINT1_IRQHandler(void)
+{
+  uint32_t regVal;
+
+  gpio1_counter++;
+  regVal = GPIOIntStatus( PORT1, 1 );
+  if ( regVal )
+  {
+	p1_1_counter++;
+	GPIOIntClear( PORT1, 1 );
+  }		
+  return;
+}
+#endif
+
+#if CONFIG_GPIO_DEFAULT_PIOINT2_IRQHANDLER==1
+volatile uint32_t gpio2_counter = 0;
+volatile uint32_t p2_1_counter  = 0;
+/*****************************************************************************
+** Function name:		PIOINT2_IRQHandler
+**
+** Descriptions:		Use one GPIO pin(port2 pin1) as interrupt source
+**
+** parameters:			None
+** Returned value:		None
+** 
+*****************************************************************************/
+void PIOINT2_IRQHandler(void)
+{
+  uint32_t regVal;
+
+  gpio2_counter++;
+  regVal = GPIOIntStatus( PORT2, 1 );
+  if ( regVal )
+  {
+	p2_1_counter++;
+	GPIOIntClear( PORT2, 1 );
+  }		
+  return;
+}
+#endif
+
+#if CONFIG_GPIO_DEFAULT_PIOINT3_IRQHANDLER==1
+volatile uint32_t gpio3_counter = 0;
+volatile uint32_t p3_1_counter  = 0;
+/*****************************************************************************
+** Function name:		PIOINT3_IRQHandler
+**
+** Descriptions:		Use one GPIO pin(port3 pin1) as interrupt source
+**
+** parameters:			None
+** Returned value:		None
+** 
+*****************************************************************************/
+void PIOINT3_IRQHandler(void)
+{
+  uint32_t regVal;
+
+  gpio3_counter++;
+  regVal = GPIOIntStatus( PORT3, 1 );
+  if ( regVal )
+  {
+	p3_1_counter++;
+	GPIOIntClear( PORT3, 1 );
+  }		
+  return;
+}
+#endif //#if CONFIG_GPIO_DEFAULT_PIOINT3_IRQHANDLER==1
+
+
+/*****************************************************************************
+** Function name:		GPIOInit
+**
+** Descriptions:		Initialize GPIO, install the
+**						GPIO interrupt handler
+**
+** parameters:			None
+** Returned value:		true or false, return false if the VIC table
+**						is full and GPIO interrupt handler can be
+**						installed.
+** 
+*****************************************************************************/
+void GPIOInit( void )
+{
+  /* Enable AHB clock to the GPIO domain. */
+  LPC_SYSCON->SYSAHBCLKCTRL |= (1<<6);
+
+#ifdef __JTAG_DISABLED  
+  LPC_IOCON->R_PIO1_1  &= ~0x07;
+  LPC_IOCON->R_PIO1_1  |= 0x01;
+#endif
+
+  /* Set up NVIC when I/O pins are configured as external interrupts. */
+#if CONFIG_GPIO_DEFAULT_PIOINT0_IRQHANDLER==1
+  NVIC_EnableIRQ(EINT0_IRQn);
+#endif
+#if CONFIG_GPIO_DEFAULT_PIOINT1_IRQHANDLER==1
+  NVIC_EnableIRQ(EINT1_IRQn);
+#endif
+#if CONFIG_GPIO_DEFAULT_PIOINT2_IRQHANDLER==1
+  NVIC_EnableIRQ(EINT2_IRQn);
+#endif
+#if CONFIG_GPIO_DEFAULT_PIOINT3_IRQHANDLER==1
+  NVIC_EnableIRQ(EINT3_IRQn);
+#endif
+  return;
+}
+
+/*****************************************************************************
+** Function name:		GPIOSetInterrupt
+**
+** Descriptions:		Set interrupt sense, event, etc.
+**						edge or level, 0 is edge, 1 is level
+**						single or double edge, 0 is single, 1 is double 
+**						active high or low, etc.
+**
+** parameters:			port num, bit position, sense, single/doube, polarity
+** Returned value:		None
+** 
+*****************************************************************************/
+void GPIOSetInterrupt( uint32_t portNum, uint32_t bitPosi, uint32_t sense,
+			uint32_t single, uint32_t event )
+{
+  switch ( portNum )
+  {
+	case PORT0:
+	  if ( sense == 0 )
+	  {
+		LPC_GPIO0->IS &= ~(0x1<<bitPosi);
+		/* single or double only applies when sense is 0(edge trigger). */
+		if ( single == 0 )
+		  LPC_GPIO0->IBE &= ~(0x1<<bitPosi);
+		else
+		  LPC_GPIO0->IBE |= (0x1<<bitPosi);
+	  }
+	  else
+	  	LPC_GPIO0->IS |= (0x1<<bitPosi);
+	  if ( event == 0 )
+		LPC_GPIO0->IEV &= ~(0x1<<bitPosi);
+	  else
+		LPC_GPIO0->IEV |= (0x1<<bitPosi);
+	break;
+ 	case PORT1:
+	  if ( sense == 0 )
+	  {
+		LPC_GPIO1->IS &= ~(0x1<<bitPosi);
+		/* single or double only applies when sense is 0(edge trigger). */
+		if ( single == 0 )
+		  LPC_GPIO1->IBE &= ~(0x1<<bitPosi);
+		else
+		  LPC_GPIO1->IBE |= (0x1<<bitPosi);
+	  }
+	  else
+	  	LPC_GPIO1->IS |= (0x1<<bitPosi);
+	  if ( event == 0 )
+		LPC_GPIO1->IEV &= ~(0x1<<bitPosi);
+	  else
+		LPC_GPIO1->IEV |= (0x1<<bitPosi);  
+	break;
+	case PORT2:
+	  if ( sense == 0 )
+	  {
+		LPC_GPIO2->IS &= ~(0x1<<bitPosi);
+		/* single or double only applies when sense is 0(edge trigger). */
+		if ( single == 0 )
+		  LPC_GPIO2->IBE &= ~(0x1<<bitPosi);
+		else
+		  LPC_GPIO2->IBE |= (0x1<<bitPosi);
+	  }
+	  else
+	  	LPC_GPIO2->IS |= (0x1<<bitPosi);
+	  if ( event == 0 )
+		LPC_GPIO2->IEV &= ~(0x1<<bitPosi);
+	  else
+		LPC_GPIO2->IEV |= (0x1<<bitPosi);  
+	break;
+	case PORT3:
+	  if ( sense == 0 )
+	  {
+		LPC_GPIO3->IS &= ~(0x1<<bitPosi);
+		/* single or double only applies when sense is 0(edge trigger). */
+		if ( single == 0 )
+		  LPC_GPIO3->IBE &= ~(0x1<<bitPosi);
+		else
+		  LPC_GPIO3->IBE |= (0x1<<bitPosi);
+	  }
+	  else
+	  	LPC_GPIO3->IS |= (0x1<<bitPosi);
+	  if ( event == 0 )
+		LPC_GPIO3->IEV &= ~(0x1<<bitPosi);
+	  else
+		LPC_GPIO3->IEV |= (0x1<<bitPosi);	  
+	break;
+	default:
+	  break;
+  }
+  return;
+}
+
+/*****************************************************************************
+** Function name:		GPIOIntEnable
+**
+** Descriptions:		Enable Interrupt Mask for a port pin.
+**
+** parameters:			port num, bit position
+** Returned value:		None
+** 
+*****************************************************************************/
+void GPIOIntEnable( uint32_t portNum, uint32_t bitPosi )
+{
+  switch ( portNum )
+  {
+	case PORT0:
+	  LPC_GPIO0->IE |= (0x1<<bitPosi); 
+	break;
+ 	case PORT1:
+	  LPC_GPIO1->IE |= (0x1<<bitPosi);	
+	break;
+	case PORT2:
+	  LPC_GPIO2->IE |= (0x1<<bitPosi);	    
+	break;
+	case PORT3:
+	  LPC_GPIO3->IE |= (0x1<<bitPosi);	    
+	break;
+	default:
+	  break;
+  }
+  return;
+}
+
+/*****************************************************************************
+** Function name:		GPIOIntDisable
+**
+** Descriptions:		Disable Interrupt Mask for a port pin.
+**
+** parameters:			port num, bit position
+** Returned value:		None
+** 
+*****************************************************************************/
+void GPIOIntDisable( uint32_t portNum, uint32_t bitPosi )
+{
+  switch ( portNum )
+  {
+	case PORT0:
+	  LPC_GPIO0->IE &= ~(0x1<<bitPosi); 
+	break;
+ 	case PORT1:
+	  LPC_GPIO1->IE &= ~(0x1<<bitPosi);	
+	break;
+	case PORT2:
+	  LPC_GPIO2->IE &= ~(0x1<<bitPosi);	    
+	break;
+	case PORT3:
+	  LPC_GPIO3->IE &= ~(0x1<<bitPosi);	    
+	break;
+	default:
+	  break;
+  }
+  return;
+}
+
+/*****************************************************************************
+** Function name:		GPIOIntStatus
+**
+** Descriptions:		Get Interrupt status for a port pin.
+**
+** parameters:			port num, bit position
+** Returned value:		None
+** 
+*****************************************************************************/
+uint32_t GPIOIntStatus( uint32_t portNum, uint32_t bitPosi )
+{
+  uint32_t regVal = 0;
+
+  switch ( portNum )
+  {
+	case PORT0:
+	  if ( LPC_GPIO0->MIS & (0x1<<bitPosi) )
+		regVal = 1;
+	break;
+ 	case PORT1:
+	  if ( LPC_GPIO1->MIS & (0x1<<bitPosi) )
+		regVal = 1;	
+	break;
+	case PORT2:
+	  if ( LPC_GPIO2->MIS & (0x1<<bitPosi) )
+		regVal = 1;		    
+	break;
+	case PORT3:
+	  if ( LPC_GPIO3->MIS & (0x1<<bitPosi) )
+		regVal = 1;		    
+	break;
+	default:
+	  break;
+  }
+  return ( regVal );
+}
+
+/*****************************************************************************
+** Function name:		GPIOIntClear
+**
+** Descriptions:		Clear Interrupt for a port pin.
+**
+** parameters:			port num, bit position
+** Returned value:		None
+** 
+*****************************************************************************/
+void GPIOIntClear( uint32_t portNum, uint32_t bitPosi )
+{
+  switch ( portNum )
+  {
+	case PORT0:
+	  LPC_GPIO0->IC |= (0x1<<bitPosi); 
+	break;
+ 	case PORT1:
+	  LPC_GPIO1->IC |= (0x1<<bitPosi);	
+	break;
+	case PORT2:
+	  LPC_GPIO2->IC |= (0x1<<bitPosi);	    
+	break;
+	case PORT3:
+	  LPC_GPIO3->IC |= (0x1<<bitPosi);	    
+	break;
+	default:
+	  break;
+  }
+  return;
+}
+
+/*****************************************************************************
+** Function name:		GPIOSetValue
+**
+** Descriptions:		Set/clear a bitvalue in a specific bit position
+**						in GPIO portX(X is the port number.)
+**
+** parameters:			port num, bit position, bit value
+** Returned value:		None
+**
+*****************************************************************************/
+void GPIOSetValue( uint32_t portNum, uint32_t bitPosi, uint32_t bitVal )
+{
+  LPC_GPIO[portNum]->MASKED_ACCESS[(1<<bitPosi)] = (bitVal<<bitPosi);
+}
+
+/*****************************************************************************
+** Function name:		GPIOSetDir
+**
+** Descriptions:		Set the direction in GPIO port
+**
+** parameters:			port num, bit position, direction (1 out, 0 input)
+** Returned value:		None
+**
+*****************************************************************************/
+void GPIOSetDir( uint32_t portNum, uint32_t bitPosi, uint32_t dir )
+{
+  if(dir)
+	LPC_GPIO[portNum]->DIR |= 1<<bitPosi;
+  else
+	LPC_GPIO[portNum]->DIR &= ~(1<<bitPosi);
+}
+#endif
+
+/******************************************************************************
+**               Functions added from the microbuilder code base
+******************************************************************************/
+
+/**************************************************************************/
+/*! 
+Software License Agreement (BSD License)
+
+Copyright (c) 2010, microBuilder SARL
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+3. Neither the name of the copyright holders nor the
+names of its contributors may be used to endorse or promote products
+derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ''AS IS'' AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+/**************************************************************************/
+/* useful functions but need to be edited to work with CMSIS
+void gpioSetPullup (volatile uint32_t *ioconReg, gpioPullupMode_t mode)
+{
+	
+	// ToDo: Disable interrupts while we are doing this?
+	
+	*ioconReg &= ~(IOCON_COMMON_MODE_MASK);
+	*ioconReg |= mode;
+	
+	// ToDo: Re-enable interrupts?
+}
+
+uint32_t gpioGetValue (uint32_t portNum, uint32_t bitPos)
+{
+	
+	uint32_t value = 0;
+	
+	switch (portNum)
+	{
+		case 0:
+			value = (GPIO_GPIO0DATA & (1 << bitPos)) ? 1 : 0;
+			break;
+		case 1:
+			value = (GPIO_GPIO1DATA & (1 << bitPos)) ? 1 : 0;
+			break;
+		case 2:
+			value = (GPIO_GPIO2DATA & (1 << bitPos)) ? 1 : 0;
+			break;
+		case 3:
+			value = (GPIO_GPIO3DATA & (1 << bitPos)) ? 1 : 0;
+			break;
+		default:
+			break;
+	}
+	
+	return value;
+}
+
+*/
+
+/******************************************************************************
+**                            End Of File
+******************************************************************************/
---
+++ src/arch/lpc11c14/drivers/uart.c
@@ -0,0 +1,411 @@
+/*****************************************************************************
+ *   uart.c:  UART API file for NXP LPC11xx Family Microprocessors
+ *
+ *   Copyright(C) 2008, NXP Semiconductor
+ *   All rights reserved.
+ *
+ *   History
+ *   2009.12.07  ver 1.00    Preliminary version, first Release
+ *
+******************************************************************************/
+#include <project/driver_config.h>
+
+#if CONFIG_ENABLE_DRIVER_UART==1
+#include <arch/uart.h>
+
+volatile uint32_t UARTStatus;
+volatile uint8_t  UARTTxEmpty = 1;
+volatile uint8_t  UARTBuffer[UART_BUFSIZE];
+volatile uint32_t UARTCount = 0;
+
+#if CONFIG_UART_DEFAULT_UART_IRQHANDLER==1
+/*****************************************************************************
+** Function name:		UART_IRQHandler
+**
+** Descriptions:		UART interrupt handler
+**
+** parameters:			None
+** Returned value:		None
+** 
+*****************************************************************************/
+void UART_IRQHandler(void)
+{
+  uint8_t IIRValue, LSRValue;
+  uint8_t Dummy = Dummy;
+
+  IIRValue = LPC_UART->IIR;
+    
+  IIRValue >>= 1;			/* skip pending bit in IIR */
+  IIRValue &= 0x07;			/* check bit 1~3, interrupt identification */
+  if (IIRValue == IIR_RLS)		/* Receive Line Status */
+  {
+    LSRValue = LPC_UART->LSR;
+    /* Receive Line Status */
+    if (LSRValue & (LSR_OE | LSR_PE | LSR_FE | LSR_RXFE | LSR_BI))
+    {
+      /* There are errors or break interrupt */
+      /* Read LSR will clear the interrupt */
+      UARTStatus = LSRValue;
+      Dummy = LPC_UART->RBR;	/* Dummy read on RX to clear 
+								interrupt, then bail out */
+      return;
+    }
+    if (LSRValue & LSR_RDR)	/* Receive Data Ready */			
+    {
+      /* If no error on RLS, normal ready, save into the data buffer. */
+      /* Note: read RBR will clear the interrupt */
+      UARTBuffer[UARTCount++] = LPC_UART->RBR;
+      if (UARTCount == UART_BUFSIZE)
+      {
+        UARTCount = 0;		/* buffer overflow */
+      }	
+    }
+  }
+  else if (IIRValue == IIR_RDA)	/* Receive Data Available */
+  {
+    /* Receive Data Available */
+    UARTBuffer[UARTCount++] = LPC_UART->RBR;
+    if (UARTCount == UART_BUFSIZE)
+    {
+      UARTCount = 0;		/* buffer overflow */
+    }
+  }
+  else if (IIRValue == IIR_CTI)	/* Character timeout indicator */
+  {
+    /* Character Time-out indicator */
+    UARTStatus |= 0x100;		/* Bit 9 as the CTI error */
+  }
+  else if (IIRValue == IIR_THRE)	/* THRE, transmit holding register empty */
+  {
+    /* THRE interrupt */
+    LSRValue = LPC_UART->LSR;		/* Check status in the LSR to see if
+								valid data in U0THR or not */
+    if (LSRValue & LSR_THRE)
+    {
+      UARTTxEmpty = 1;
+    }
+    else
+    {
+      UARTTxEmpty = 0;
+    }
+  }
+  return;
+}
+#endif
+
+/*****************************************************************************
+** Function name:		ModemInit
+**
+** Descriptions:		Initialize UART0 port as modem, setup pin select.
+**
+** parameters:			None
+** Returned value:		None
+** 
+*****************************************************************************/
+void ModemInit( void )
+{
+  LPC_IOCON->PIO2_0 &= ~0x07;    /* UART I/O config */
+  LPC_IOCON->PIO2_0 |= 0x01;     /* UART DTR */
+  LPC_IOCON->PIO0_7 &= ~0x07;    /* UART I/O config */
+  LPC_IOCON->PIO0_7 |= 0x01;     /* UART CTS */
+  LPC_IOCON->PIO1_5 &= ~0x07;    /* UART I/O config */
+  LPC_IOCON->PIO1_5 |= 0x01;     /* UART RTS */
+#if 1 
+  LPC_IOCON->DSR_LOC	= 0;
+  LPC_IOCON->PIO2_1 &= ~0x07;    /* UART I/O config */
+  LPC_IOCON->PIO2_1 |= 0x01;     /* UART DSR */
+
+  LPC_IOCON->DCD_LOC	= 0;
+  LPC_IOCON->PIO2_2 &= ~0x07;    /* UART I/O config */
+  LPC_IOCON->PIO2_2 |= 0x01;     /* UART DCD */
+
+  LPC_IOCON->RI_LOC	= 0;
+  LPC_IOCON->PIO2_3 &= ~0x07;    /* UART I/O config */
+  LPC_IOCON->PIO2_3 |= 0x01;     /* UART RI */
+
+#else
+  LPC_IOCON->DSR_LOC = 1;
+  LPC_IOCON->PIO3_1 &= ~0x07;    /* UART I/O config */
+  LPC_IOCON->PIO3_1 |= 0x01;     /* UART DSR */
+
+  LPC_IOCON->DCD_LOC = 1;
+  LPC_IOCON->PIO3_2 &= ~0x07;    /* UART I/O config */
+  LPC_IOCON->PIO3_2 |= 0x01;     /* UART DCD */
+
+  LPC_IOCON->RI_LOC = 1;
+  LPC_IOCON->PIO3_3 &= ~0x07;    /* UART I/O config */
+  LPC_IOCON->PIO3_3 |= 0x01;     /* UART RI */
+#endif
+  LPC_UART->MCR = 0xC0;          /* Enable Auto RTS and Auto CTS. */			
+  return;
+}
+
+/*****************************************************************************
+** Function name:		UARTInit
+**
+** Descriptions:		Initialize UART0 port, setup pin select,
+**				clock, parity, stop bits, FIFO, etc.
+**
+** parameters:			UART baudrate
+** Returned value:		None
+** 
+*****************************************************************************/
+void UARTInit(uint32_t baudrate)
+{
+  uint32_t Fdiv;
+  uint32_t regVal;
+
+  UARTTxEmpty = 1;
+  UARTCount = 0;
+  
+  NVIC_DisableIRQ(UART_IRQn);
+
+  LPC_IOCON->PIO1_6 &= ~0x07;    /*  UART I/O config */
+  LPC_IOCON->PIO1_6 |= 0x01;     /* UART RXD */
+  LPC_IOCON->PIO1_7 &= ~0x07;	
+  LPC_IOCON->PIO1_7 |= 0x01;     /* UART TXD */
+  /* Enable UART clock */
+  LPC_SYSCON->SYSAHBCLKCTRL |= (1<<12);
+  LPC_SYSCON->UARTCLKDIV = 0x1;     /* divided by 1 */
+
+  LPC_UART->LCR = 0x83;             /* 8 bits, no Parity, 1 Stop bit */
+  regVal = LPC_SYSCON->UARTCLKDIV;
+
+  Fdiv = (((SystemCoreClock*LPC_SYSCON->SYSAHBCLKDIV)/regVal)/16)/baudrate ;	/*baud rate */
+
+  LPC_UART->DLM = Fdiv / 256;							
+  LPC_UART->DLL = Fdiv % 256;
+  LPC_UART->LCR = 0x03;		/* DLAB = 0 */
+  LPC_UART->FCR = 0x07;		/* Enable and reset TX and RX FIFO. */
+
+  /* Read to clear the line status. */
+  regVal = LPC_UART->LSR;
+
+  /* Ensure a clean start, no data in either TX or RX FIFO. */
+// CodeRed - added parentheses around comparison in operand of &
+  while (( LPC_UART->LSR & (LSR_THRE|LSR_TEMT)) != (LSR_THRE|LSR_TEMT) );
+  while ( LPC_UART->LSR & LSR_RDR )
+  {
+	regVal = LPC_UART->RBR;	/* Dump data from RX FIFO */
+  }
+ 
+  /* Enable the UART Interrupt */
+  NVIC_EnableIRQ(UART_IRQn);
+
+#if CONFIG_UART_ENABLE_INTERRUPT==1
+#if CONFIG_UART_ENABLE_TX_INTERRUPT==1
+  LPC_UART->IER = IER_RBR | IER_THRE | IER_RLS;	/* Enable UART interrupt */
+#else
+  LPC_UART->IER = IER_RBR | IER_RLS;	/* Enable UART interrupt */
+#endif
+#endif
+  return;
+}
+
+/*****************************************************************************
+** Function name:		UARTSend
+**
+** Descriptions:		Send a block of data to the UART 0 port based
+**				on the data length
+**
+** parameters:		buffer pointer, and data length
+** Returned value:	None
+** 
+*****************************************************************************/
+void UARTSend(uint8_t *BufferPtr, uint32_t Length)
+{
+  
+  while ( Length != 0 )
+  {
+	  /* THRE status, contain valid data */
+#if CONFIG_UART_ENABLE_TX_INTERRUPT==1
+	  /* Below flag is set inside the interrupt handler when THRE occurs. */
+      while ( !(UARTTxEmpty & 0x01) );
+	  LPC_UART->THR = *BufferPtr;
+      UARTTxEmpty = 0;	/* not empty in the THR until it shifts out */
+#else
+	  while ( !(LPC_UART->LSR & LSR_THRE) );
+	  LPC_UART->THR = *BufferPtr;
+#endif
+      BufferPtr++;
+      Length--;
+  }
+  return;
+}
+#endif
+
+static void UART_putchar(int c) {
+	uint8_t c1 = c;
+	UARTSend(&c1, 1);
+}
+
+#define putchar(c) UART_putchar(c)
+
+static void UART_printchar(char **str, int c)
+{
+	if (str) {
+		**str = c;
+		++(*str);
+	} else
+		(void)UART_putchar(c);
+}
+
+#define PAD_RIGHT 1
+#define PAD_ZERO 2
+
+static int UART_prints(char **out, const char *string, int width, int pad)
+{
+	register int pc = 0, padchar = ' ';
+
+	if (width > 0) {
+		register int len = 0;
+		register const char *ptr;
+		for (ptr = string; *ptr; ++ptr) ++len;
+		if (len >= width) width = 0;
+		else width -= len;
+		if (pad & PAD_ZERO) padchar = '0';
+	}
+	if (!(pad & PAD_RIGHT)) {
+		for ( ; width > 0; --width) {
+			UART_printchar (out, padchar);
+			++pc;
+		}
+	}
+	for ( ; *string ; ++string) {
+		UART_printchar (out, *string);
+		++pc;
+	}
+	for ( ; width > 0; --width) {
+		UART_printchar (out, padchar);
+		++pc;
+	}
+
+	return pc;
+}
+
+/* the following should be enough for 32 bit int */
+#define PRINT_BUF_LEN 12
+
+static int UART_printi(char **out, int i, int b, int sg, int width, int pad, int letbase)
+{
+	char print_buf[PRINT_BUF_LEN];
+	register char *s;
+	register int t, neg = 0, pc = 0;
+	register unsigned int u = i;
+
+	if (i == 0) {
+		print_buf[0] = '0';
+		print_buf[1] = '\0';
+		return UART_prints (out, print_buf, width, pad);
+	}
+
+	if (sg && b == 10 && i < 0) {
+		neg = 1;
+		u = -i;
+	}
+
+	s = print_buf + PRINT_BUF_LEN-1;
+	*s = '\0';
+
+	while (u) {
+		t = u % b;
+		if( t >= 10 )
+			t += letbase - '0' - 10;
+		*--s = t + '0';
+		u /= b;
+	}
+
+	if (neg) {
+		if( width && (pad & PAD_ZERO) ) {
+			UART_printchar (out, '-');
+			++pc;
+			--width;
+		}
+		else {
+			*--s = '-';
+		}
+	}
+
+	return pc + UART_prints (out, s, width, pad);
+}
+
+static int UART_print(char **out, int *varg)
+{
+	register int width, pad;
+	register int pc = 0;
+	register char *format = (char *)(*varg++);
+	char scr[2];
+
+	for (; *format != 0; ++format) {
+		if (*format == '%') {
+			++format;
+			width = pad = 0;
+			if (*format == '\0') break;
+			if (*format == '%') goto out;
+			if (*format == '-') {
+				++format;
+				pad = PAD_RIGHT;
+			}
+			while (*format == '0') {
+				++format;
+				pad |= PAD_ZERO;
+			}
+			for ( ; *format >= '0' && *format <= '9'; ++format) {
+				width *= 10;
+				width += *format - '0';
+			}
+			if( *format == 's' ) {
+				register char *s = *((char **)varg++);
+				pc += UART_prints (out, s?s:"(null)", width, pad);
+				continue;
+			}
+			if( *format == 'd' ) {
+				pc += UART_printi (out, *varg++, 10, 1, width, pad, 'a');
+				continue;
+			}
+			if( *format == 'x' ) {
+				pc += UART_printi (out, *varg++, 16, 0, width, pad, 'a');
+				continue;
+			}
+			if( *format == 'X' ) {
+				pc += UART_printi (out, *varg++, 16, 0, width, pad, 'A');
+				continue;
+			}
+			if( *format == 'u' ) {
+				pc += UART_printi (out, *varg++, 10, 0, width, pad, 'a');
+				continue;
+			}
+			if( *format == 'c' ) {
+				/* char are converted to int then pushed on the stack */
+				scr[0] = *varg++;
+				scr[1] = '\0';
+				pc += UART_prints (out, scr, width, pad);
+				continue;
+			}
+		}
+		else {
+		out:
+			UART_printchar (out, *format);
+			++pc;
+		}
+	}
+	if (out) **out = '\0';
+	return pc;
+}
+
+/* assuming sizeof(void *) == sizeof(int) */
+
+int UART_printf(const char *format, ...)
+{
+	register int *varg = (int *)(&format);
+	return UART_print(0, varg);
+}
+
+int UART_sprintf(char *out, const char *format, ...)
+{
+	register int *varg = (int *)(&format);
+	return UART_print(&out, varg);
+}
+/******************************************************************************
+**                            End Of File
+******************************************************************************/

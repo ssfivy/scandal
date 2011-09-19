ddc9976e4d21f173e18c962a8ccd9c753e194c0f
Etienne Le Sueur <elesueur@cse.unsw.edu.au>
2011-09-19 23:29:41 +1000

    flesh out wavesculptor code

--- include/scandal/wavesculptor.h
+++ include/scandal/wavesculptor.h
@@ -2,8 +2,18 @@
 #include <scandal/can.h>
 #include <scandal/engine.h>
 
+typedef			void (*ws_base_callback)(char *tritium_id, uint32_t serial_number, uint32_t time);
+void			scandal_register_ws_base_callback(ws_base_callback cb);
+
+typedef			void (*ws_status_callback)(uint8_t rcv_err_count, uint8_t tx_err_count, uint16_t active_motor,
+						uint16_t err_flags, uint16_t limit_flags, uint32_t time);
+void			scandal_register_ws_base_callback(ws_base_callback cb);
+
 typedef			void (*ws_bus_callback)(float bus_current, float bus_voltage, uint32_t time);
 void			scandal_register_ws_bus_callback(ws_bus_callback cb);
+
+typedef			void (*ws_velocity_callback)(float vehicle_velocity, float motor_rpm uint32_t time);
+void			scandal_register_ws_velocity_callback(ws_velocity_callback cb);
 
 typedef			void (*ws_temp_callback)(float hs_temp, float motor_temp, uint32_t time);
 void			scandal_register_ws_temp_callback(ws_temp_callback cb);
--- src/drivers/wavesculptor.c
+++ src/drivers/wavesculptor.c
@@ -5,12 +5,30 @@
 #include <scandal/can.h>
 #include <scandal/stdio.h>
 
-ws_bus_callback   ws_bus_handler_cb = 0;
-ws_temp_callback   ws_temp_handler_cb = 0;
+ws_base_callback ws_base_handler_cb = 0;
+ws_status_callback ws_status_handler_cb = 0;
+ws_bus_callback ws_bus_handler_cb = 0;
+ws_velocity_callback ws_velocity_handler_cb = 0;
+ws_temp_callback ws_temp_handler_cb = 0;
+
+void scandal_register_ws_base_callback(ws_base_callback cb) {
+	can_register_id(CAN_ID_STD_MASK, MC_BASE, 0, CAN_STD_MSG);
+	ws_base_handler_cb = cb;
+}
+
+void scandal_register_ws_status_callback(ws_status_callback cb) {
+	can_register_id(CAN_ID_STD_MASK, MC_STATUS, 0, CAN_STD_MSG);
+	ws_status_handler_cb = cb;
+}
 
 void scandal_register_ws_bus_callback(ws_bus_callback cb) {
 	can_register_id(CAN_ID_STD_MASK, MC_BUS, 0, CAN_STD_MSG);
 	ws_bus_handler_cb = cb;
+}
+
+void scandal_register_ws_velocity_callback(ws_velocity_callback cb) {
+	can_register_id(CAN_ID_STD_MASK, MC_VELOCITY, 0, CAN_STD_MSG);
+	ws_velocity_handler_cb = cb;
 }
 
 void scandal_register_ws_temp_callback(ws_temp_callback cb) {
@@ -20,10 +38,38 @@ void scandal_register_ws_temp_callback(ws_temp_callback cb) {
 
 void scandal_handle_ws_message(can_msg *msg) {
 	switch(msg->id) {
+	 case MC_BASE:
+		if (ws_base_handler_cb != 0) {
+			group_64 *data = (group_64 *)msg->data;
+			ws_base_handler_cb((char *)(data->data_u32[1]), data->data_u32[0],
+						sc_get_timer());
+		}
+		break;
+
+	 case MC_STATUS:
+		if (ws_bus_handler_cb != 0) {
+			group_64 *data = (group_64 *)msg->data;
+			int8_t rcv_err_count =  data->data_u8[7];
+			uint8_t tx_err_count =  data->data_u8[6];
+			uint16_t active_motor = data->data_u16[2];
+			uint16_t err_flags =    data->data_u16[1];
+			uint16_t limit_flags =  data->data_u16[0];
+			ws_status_handler_cb(rcv_err_count, tx_err_count, active_motor,
+						err_flags, limit_flags, sc_get_timer());
+		}
+		break;
+
 	 case MC_BUS:
 		if (ws_bus_handler_cb != 0) {
 			group_64 *data = (group_64 *)msg->data;
 			ws_bus_handler_cb(data->data_fp[1], data->data_fp[0], sc_get_timer());
+		}
+		break;
+
+	 case MC_VELOCITY:
+		if (ws_velocity_handler_cb != 0) {
+			group_64 *data = (group_64 *)msg->data;
+			ws_velocity_handler_cb(data->data_fp[1], data->data_fp[0], sc_get_timer());
 		}
 		break;
 
